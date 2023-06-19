df2303d89f8f38af54db31ad35f6ef3b041a57cd
Vuxxs <57200277+Vuxxs@users.noreply.github.com>
2023-06-19 20:30:12 +0300

    Structure Changes
    - Remove default app folder
    - Create a basic _app.tsx
    - Create a hello world index
    - Move global.css to styles
    - Fix global.css imports

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-200">
+      <h1 className="text-4xl text-blue-600">Hello, World!</h1>
+    </div>
+  );
+}
