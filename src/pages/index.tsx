2b3000692072cfd5772042c14d5e0bcb9506d5d6
Vuxxs <57200277+Vuxxs@users.noreply.github.com>
2023-06-20 15:04:41 +0300

    changed style
    - added border colour to login button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,8 @@ export default function Home() {
       {/* Nearest colour to angelic marian blue*/}
       <header className="w-full flex justify-end p-5">
         <a
-          href="https://discord.com/api/oauth2/authorize?client_id=1107632678829817956&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code&scope=identify%20guilds"
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          href="https://discord.com/api/oauth2/authorize?client_id=1107632678829817956&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fhome&response_type=code&scope=identify%20guilds"
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded border-white border-2"
         >
           Login to Discord
         </a>
