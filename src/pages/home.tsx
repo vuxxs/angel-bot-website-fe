fcdb5b4941de1b96c0554422202f24357375d006
Vuxxs <57200277+Vuxxs@users.noreply.github.com>
2023-06-21 17:01:39 +0300

    GuildButton Components to /home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,8 +2,11 @@ import { useEffect } from "react";
 import UserHeader from "@/components/UserHeader";
 import CenterLogo from "@/components/Logo";
 import Layout from "@/components/Layout";
+import { Guild, User } from "discord.js";
+import GuildButton from "@/components/GuildButton";
 
-const HomePage = ({ user }: any) => {
+// FIXME Update user type to User when global_name is released by discord.js team
+const HomePage = ({ user, guilds }: { user: any; guilds: Guild[] }) => {
   const username = user?.global_name;
   const avatarURL = `https://cdn.discordapp.com/avatars/${user?.id}/${user?.avatar}.png?size=4096`;
 
@@ -17,6 +20,11 @@ const HomePage = ({ user }: any) => {
     <Layout>
       <UserHeader username={username} avatarURL={avatarURL} />
       <CenterLogo />
+      <div className="flex flex-wrap justify-center">
+        {guilds.map((guild) => (
+          <GuildButton key={guild.id} guild={guild} />
+        ))}
+      </div>
     </Layout>
   );
 };
@@ -48,9 +56,12 @@ export async function getServerSideProps(context: any) {
     }
     const json = await response.json();
 
+    console.log(json.guilds);
+
     return {
       props: {
         user: json.user,
+        guilds: json.guilds,
       },
     };
   } catch (error) {
