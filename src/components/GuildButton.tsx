5ac953e0f7116eaef3f9dd47f98739c3a42d2a1f
Vuxxs <57200277+Vuxxs@users.noreply.github.com>
2023-06-21 17:01:06 +0300

    GuildButton Redesign

--- src/components/GuildButton.tsx
+++ src/components/GuildButton.tsx
@@ -4,14 +4,22 @@ import Link from "next/link";
 
 const GuildButton = ({ guild }: { guild: Guild }) => {
   const { id, name, icon } = guild;
-  const iconUrl = `https://cdn.discordapp.com/icons/${id}/${icon}.png`;
+  const iconUrl = icon
+    ? `https://cdn.discordapp.com/icons/${id}/${icon}.png`
+    : "/temp-default-server-icon.png"; // FIXME Update to default-server-icon.png
 
   return (
     <Link href={`/guild/${id}`}>
-      <a className="flex items-center justify-center p-4 m-4 rounded-full bg-blue-500 hover:bg-blue-700">
-        <Image src={iconUrl} alt={name} width={64} height={64} />
+      <div className="flex items-center justify-center p-4 m-4 rounded-full bg-slate-700 hover:bg-slate-600 border-green-600 border-2">
+        <Image
+          className="rounded-full"
+          src={iconUrl}
+          alt={name}
+          width={64}
+          height={64}
+        />
         <span className="ml-4 text-white">{name}</span>
-      </a>
+      </div>
     </Link>
   );
 };
