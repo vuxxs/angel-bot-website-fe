c2597128496ed0959ba3a9fc2f9828312fca2650
Vuxxs <57200277+Vuxxs@users.noreply.github.com>
2023-06-21 16:18:51 +0300

    Renamed & Redesigned error.tsx

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import BackToIndexButton from "@/components/BackToIndexButton";
+import Layout from "@/components/Layout";
+
+const Custom404 = () => {
+  return (
+    <Layout>
+      <BackToIndexButton />
+    </Layout>
+  );
+};
+
+export default Custom404;
