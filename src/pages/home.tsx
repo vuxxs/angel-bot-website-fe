9a14c9682fbe7a52b7ed40e77d490582951de43d
Vuxxs <57200277+Vuxxs@users.noreply.github.com>
2023-06-21 15:43:12 +0300

    Components Usage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,50 +1,21 @@
 import { useEffect } from "react";
-import Image from "next/image";
-import Link from "next/link";
+import UserHeader from "@/components/UserHeader";
+import CenterLogo from "@/components/Logo";
 
 const HomePage = ({ user }: any) => {
   const username = user?.global_name;
   const avatarURL = `https://cdn.discordapp.com/avatars/${user?.id}/${user?.avatar}.png?size=4096`;
-  const guilds = user?.guilds;
 
   useEffect(() => {
-    if (typeof window != "undefined")
+    if (typeof window != "undefined") {
       window.history.replaceState({}, document.title, "/home");
-  });
+    }
+  }, []); // No dependencies, run only once
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-indigo-800">
-      <header className="w-full flex justify-end p-5">
-        <Link href="/">
-          <div className="flex items-center border-2 border-white bg-red-500 hover:bg-red-700 rounded-full p-2">
-            <div>
-              <div className="text-white font-bold mr-2">
-                Logged in as {username || "user"}
-              </div>
-            </div>
-            <div className="w-16 h-16 rounded-full overflow-hidden">
-              <Image
-                src={avatarURL || "/default-user.logo.png"}
-                alt="Logo"
-                className="mx-auto"
-                width={64}
-                height={64}
-              />
-            </div>
-          </div>
-        </Link>
-      </header>
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <div className="text-center">
-          <Image
-            src="/temp-logo.png"
-            alt="Logo"
-            className="mx-auto"
-            width={150}
-            height={150}
-          />
-          <h1 className="text-6xl text-white font-bold">Angel Bot</h1>
-        </div>
-      </main>
+      <UserHeader username={username} avatarURL={avatarURL} />
+      <CenterLogo />
     </div>
   );
 };
@@ -62,14 +33,34 @@ export async function getServerSideProps(context: any) {
     };
   }
 
-  const response = await fetch(`http://localhost:3001/login?code=${code}`);
-  const json = await response.json();
+  try {
+    const response = await fetch(`http://localhost:3001/login?code=${code}`);
 
-  return {
-    props: {
-      user: json.user,
-    },
-  };
+    if (!response.ok) {
+      // console.error("Server responded with an error", response.status);
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+    const json = await response.json();
+
+    return {
+      props: {
+        user: json.user,
+      },
+    };
+  } catch (error) {
+    console.error("An error occurred while fetching user data", error);
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
 }
 
 export default HomePage;
