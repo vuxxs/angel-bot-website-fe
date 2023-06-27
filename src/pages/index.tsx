a5f854ddcda9bfef206361cba23b7586c9c81d25
Vuxxs <57200277+Vuxxs@users.noreply.github.com>
2023-06-27 21:26:07 +0300

    Header Component
    - Header Component
    - Updated Index to use Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import Layout from "@/components/Layout";
 import LoginButton from "@/components/LoginButton";
 import CenterLogo from "@/components/Logo";
@@ -5,7 +6,7 @@ import CenterLogo from "@/components/Logo";
 export default function Home() {
   return (
     <Layout>
-      <LoginButton />
+      <Header />
       <CenterLogo />
     </Layout>
   );
