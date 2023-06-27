e265f3b2fa884899d592258cf7550df443008ebb
Vuxxs <57200277+Vuxxs@users.noreply.github.com>
2023-06-27 21:32:27 +0300

    Updated Index
    - Removed unnecessary import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Header from "@/components/Header";
 import Layout from "@/components/Layout";
-import LoginButton from "@/components/LoginButton";
 import CenterLogo from "@/components/Logo";
 
 export default function Home() {
