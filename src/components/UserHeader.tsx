31d52e1dbde2ccf4b9eabfca8a43cd83c266a9ad
Vuxxs <57200277+Vuxxs@users.noreply.github.com>
2023-06-28 09:40:48 +0300

    Changed UserHeader Component
    Removed unnecessary clutter & made to the size of Usage Button

--- src/components/UserHeader.tsx
+++ src/components/UserHeader.tsx
@@ -8,27 +8,21 @@ const UserHeader = ({
   username: string;
   avatarURL: string;
 }) => (
-  <header className="w-full flex justify-end p-5">
-    <Link href="/">
-      <div className="flex items-center border-2 border-white bg-red-500 hover:bg-red-700 rounded-full p-2">
-        <div>
-          <div className="text-white font-bold mr-2">
-            Logged in as {username || "user"}
-          </div>
-        </div>
-        <div className="w-16 h-16 rounded-full overflow-hidden">
-          <Image
-            // FIXME Update to default-user-logo.png
-            src={avatarURL || "/temp-default-user-logo.png"}
-            alt="Logo"
-            className="mx-auto"
-            width={64}
-            height={64}
-          />
-        </div>
+  <Link href="/">
+    <div className="bg-red-500 hover:bg-red-700 flex items-center border-2 border-white rounded-full p-2 h-11">
+      <div className="text-white font-bold mr-2">
+        Logged in as {username || "user"}
       </div>
-    </Link>
-  </header>
+      <div className="w-8 h-8 rounded-full overflow-hidden relative">
+        <Image
+          // FIXME Update to default-user-logo.png
+          src={avatarURL || "/temp-default-user-logo.png"}
+          alt="Logo"
+          fill
+        />
+      </div>
+    </div>
+  </Link>
 );
 
 export default UserHeader;
