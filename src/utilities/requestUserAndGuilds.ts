608f939d52a9b0f070288f3b63bb4e05302eb529
Vuxxs <57200277+Vuxxs@users.noreply.github.com>
2023-06-29 13:04:51 +0300

    Rename Utility

--- src/utilities/requestUserAndGuilds.ts
+++ src/utilities/requestUserAndGuilds.ts
@@ -1,4 +1,4 @@
-export default async function requestUser(context: any) {
+export default async function requestUserAndGuilds(context: any) {
   try {
     const cookie = context.req.headers.cookie;
     const response = await fetch("http://localhost:3001/session", {
